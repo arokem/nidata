578966d4896d26e7c048398feb18bf3514d9cce4
arokem <arokem@gmail.com>
2015-06-13 15:20:10 -1000

    RF: Just some stuff.

--- nidata/fetchers/aws_fetcher.py
+++ nidata/fetchers/aws_fetcher.py
@@ -16,13 +16,33 @@ def test_cb(cur_bytes, total_bytes, t0=None, **kwargs):
     return _chunk_report_(bytes_so_far=cur_bytes, total_size=total_bytes, initial_size=0, t0=t0)
 
 
-def aws_fetcher(data_dir, files, access_key=None, secret_access_key=None, profile_name=None, force=True):
+def aws_fetcher(data_dir, files, access_key_id=None, secret_access_key=None, 
+                profile_name=None, force=True):
+    """
+    Fetch data from Amazon Web Services (AWS) Simple Storage Service (S3)
+
+    Parameters
+    ----------
+    data_dir : str
+        The 
+    files : tuple
+        (local_name, remote_name, opts)
+    access_key_id : str, optional
+    secret_access_key : str, optional
+    profile_name : str, optional
+    force :
+
+    """
     if profile_name is not None:
         s3 = boto.connect_s3(profile_name=profile_name)
-    elif access_key is not None and secret_access_key is not None:
-        s3 = boto.connect_s3(access_key, secret_access_key)
-
-
+    elif access_key_id is not None and secret_access_key is not None:
+        s3 = boto.connect_s3(access_key_id, secret_access_key)
+    if (access_key_id is None and secret_access_key is None and profile_name
+        is None):
+        w_s = "No AWS profile or credentials specified, defaulting to your"  
+        w_s += "environment-set AWS credentials"
+        warnings.warn(w_s)
+        
     bucket_names = np.unique([opts.get('bucket') for f, rk, opts in files])
     for bucket_name in bucket_names:  # loop over bucket names for efficiency.
         if bucket_name:
@@ -41,6 +61,9 @@ def aws_fetcher(data_dir, files, access_key=None, secret_access_key=None, profil
                 files_.append(None)
             elif force or not os.path.exists(target_file):
                 with open(target_file, 'wb') as fp:
-                    key.get_contents_to_file(fp, cb=partial(test_cb, t0=time.time()), num_cb=None)
+                    key.get_contents_to_file(fp, cb=partial(test_cb,
+                                             t0=time.time()), 
+                                             num_cb=None)
                 files_.append(target_file)
+
     return files_
